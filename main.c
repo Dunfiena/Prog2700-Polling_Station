a1cb0771792c144b0b1d3d36d42792a50b4cfb93
Dunfiena <117761149+Dunfiena@users.noreply.github.com>
2023-03-14 12:51:56 -0300

    Commit 2 march 13 2023

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <String.h>
-#include <stdlib.h>
 
 struct Voter{
-    int voterID;
     char FirstName[50];
     char LastName[50];
-    long RegistrationNumber;
+    int RegistrationNumber;
     int DoB;
     bool hasVoted;
 };
@@ -34,9 +32,11 @@ int main() {
     struct Admin sa[1];
     struct Candidate c[5];
     char userentry[50], userentry2[50], userentry3[50];
-    int path, n, result, result2, result3;
+    int path, result, result2, result3;
+    int POadd=0, p=0, j=0, k=0;
+    int candidateadd=0;
+
     bool flag=true;
-    struct voter *voters=(struct voter*)malloc(n*sizeof(struct Voter));
     while (flag == true) {
         int entry;
         printf("\nWelcome to the voter registration kiosk"
@@ -52,63 +52,105 @@ int main() {
                 //polling officer login
                 printf("\nLogin in\n---------------"
                        "\nPlease enter your first name");
-                scanf("%s", &userentry);
+                scanf("%s", userentry);
                 printf("\nPlease enter your lastname");
-                scanf("%s", &userentry2);
+                scanf("%s", userentry2);
                 printf("\nPlease enter your password");
-                scanf("%s", &userentry3);
-                for(int l=0; l<3;l++){
+                scanf("%s", userentry3);
+                for(int l=0; l<3;l++) {
                     result = strcmp(userentry, po[l].FirstName);
                     result2 = strcmp(userentry2, po[l].LastName);
                     result3 = strcmp(userentry3, po[l].Password);
-                    if (result == 0 && result2==0 && result3==0){
+                    if (result == 0 && result2 == 0 && result3 == 0) {
                         printf("\n You have successfully logged in");
-                        printf("\nPlease select option: (1) Register Voter (2)Declare Results (3) Cancel");
-                        scanf("%d", &path);
-                        if (path==1) {
-                            //enter voter
-                            int numOfEntries = 0;
-                            numOfEntries += 1;
-                            for (int j = 0; j < numOfEntries; j++) {
-                                struct Voter person;
-                                printf("\nPlease enter voter's First Name");
-                                scanf("%s", v[j].FirstName);
-                                printf("\nPlease enter voter's Last Name");
-                                scanf("%s", v[j].LastName);
-                                printf("\nPlease enter voter's Date of Birth (as yyyymmdd");
-                                scanf("%d", &v[j].DoB);
-                                v[j].RegistrationNumber = 11223344;
+                        bool poSession=true;
+                        while(poSession) {
+                            printf("\nPlease select option: (1) Register Voter (2) Add Candidate (3)Declare Results (4) Cancel");
+                            scanf("%d", &path);
+                            if (path == 1) {
+                                //enter voter
+                                int numOfEntries = 0;
+                                numOfEntries += 1;
+                                for (j; j < numOfEntries; j++) {
+                                    printf("\nPlease enter voter's First Name");
+                                    scanf("%s", v[j].FirstName);
+                                    printf("\nPlease enter voter's Last Name");
+                                    scanf("%s", v[j].LastName);
+                                    printf("\nPlease enter voter's Date of Birth (as yyyymmdd)");
+                                    scanf("%d", &v[j].DoB);
+                                    printf("\nPlease enter voter's 8 digit registration number");
+                                    scanf("%d", &v[j].RegistrationNumber);
+                                }
+                            } else if (path == 2) {
+                                candidateadd += 1;
+                                if (candidateadd == 5) {
+                                    printf("You have reached the maximum number of candidates in the system.");
+                                } else {
+                                    for (p; p < candidateadd; p++) {
+                                        c[1].candidateID = candidateadd;
+                                        printf("\nPlease enter the candidates First Name");
+                                        scanf("%s", c[p].FirstName);
+                                        printf("\nPlease enter the candidates Last Name");
+                                        scanf("%s", c[p].LastName);
+                                    }
+                                }
+                            } else if (path == 3) {
+                                //END OF SESSION RESULTS OUTPUT
+                                printf("\nDeclaring the results will end the session. Would you like to proceed? (1) yes (2) no");
+                                scanf("%d", &path);
+                                if (path == 1) {
+                                    flag = false;
+                                    poSession=false;
+                                    int mostvotes = 0;
+                                    int times = 0;
+                                    int winner;
+                                    for (int q = 0; q <= 4; q++) {
+                                        if (c[q].candidateID == q) {
+                                            printf("\n%s %s has a total of %d votes", c[q].FirstName, c[q].LastName,
+                                                   c[q].Votes);
+                                            times += 1;
+                                            int votes = c[q].Votes;
+                                            if (votes > mostvotes) {
+                                                mostvotes = votes;
+                                                winner = q;
+                                            }
+                                        } else if (q == times + 1) {
+                                            printf("\nThe results of this election as as follows"
+                                                   "\n------------------------------------------"
+                                                   "\nWinner of the election is %s %s with %d votes",
+                                                   c[winner].FirstName,
+                                                   c[winner].LastName, c[winner].Votes);
+                                        } else {
+                                            printf("\nEnd of Session, thank you for your service.");
+                                        }
+                                    }
+                                }
+                            } else {
+                                poSession=false;
                             }
-                        }else{
-                            printf("\nInformation was incorrect");
                         }
-                    }else if(path==2){
-                        //END OF SESSION RESULTS OUTPUT
-                        printf("\nDeclaring the results will end the session. Would you like to proceed? (1) yes (2) no");
-                        scanf("%d", &path);
-                        if(path==1){
-                            flag=false;
-                        }
+                    } else {
+                        printf("\nInformation was incorrect");
                     }
                 }
                 //end of PO section
 
                 //System Admin path
             } else if (path == 2) {
-                if(sa[1].AdminID==1){
+                if(sa[0].AdminID==1){
                     bool loginflag=false;
                     while(!loginflag) {
                         printf("\nPlease enter your username");
-                        scanf("%s", &userentry);
-                        result = strcmp(userentry, sa[1].LoginID);
+                        scanf("%s", userentry);
+                        result = strcmp(userentry, sa[0].LoginID);
                         if (result == 0) {
                             printf("\nPlease enter your password");
-                            scanf("%s", &userentry);
-                            result = strcmp(userentry, sa[1].Password);
+                            scanf("%s", userentry);
+                            result = strcmp(userentry, sa[0].Password);
                             if (result == 0) {
-                                int POadd;
+
                                 printf("\nYou have logged in\nPolling officer count is currently at %d."
-                                       "\n Would you like to add a polling officer (1) or candidate (2)", POadd);
+                                       "\n Would you like to add a polling officer (1) or cancel (2)", POadd);
                                 scanf("%d",&path);
                                 if(path==1) {
                                     loginflag = true;
@@ -119,7 +161,7 @@ int main() {
                                     }
                                     bool adding = false;
                                     while (!adding)
-                                        for (int k = 0; k < POadd; k++) {
+                                        for (k; k < POadd; k++) {
                                             po[1].officerID = POadd;
                                             printf("\nPlease enter the Polling officers First Name");
                                             scanf("%s", po[k].FirstName);
@@ -127,25 +169,13 @@ int main() {
                                             scanf("%s", po[k].LastName);
                                             printf("\nPlease enter the Polling officers Password");
                                             scanf("%s", po[k].Password);
-                                            printf("%s %s has been added, would you like to add another polling officer? (1) yes (2) no");
+                                            printf("%s %s has been added, would you like to add another polling officer? (1) yes (2) no"
+                                                    , po[k].FirstName, po[k].LastName);
                                             scanf("%d", &path);
                                             if (path == 1) {
+
                                             } else { adding = true; }
                                         }
-                                }else if(path==2) {
-                                    int candidateadd;
-                                    candidateadd+=1;
-                                    if(candidateadd==5){
-                                        printf("You have reached the maximum number of candidates in the system.");
-                                    }else{
-                                    for (int p = 0; p < candidateadd; p++) {
-                                        c[1].candidateID = candidateadd;
-                                        printf("\nPlease enter the candidates First Name");
-                                        scanf("%s", c[p].FirstName);
-                                        printf("\nPlease enter the candidates Last Name");
-                                        scanf("%s", c[p].LastName);
-                                    }
-                                    }
                                 }
                             } else {
                                 printf("\nPassword  incorrect, please try again");
@@ -155,11 +185,11 @@ int main() {
                         }
                     }
                 }else{
-                    sa[1].AdminID=1;
+                    sa[0].AdminID=1;
                     printf("\nHello Admin, please set your username");
-                    scanf("%s", sa[1].LoginID);
+                    scanf("%s", sa[0].LoginID);
                     printf("\nPlease set your password");
-                    scanf("%s",sa[1].Password);
+                    scanf("%s",sa[0].Password);
                 }
             }
 
